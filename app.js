85369f956c7eb034340365945aca9d90203681e8
bangmun07-source <bangmun07@gmail.com>
2026-08-07 16:41:32 +0700

    Update app.js

--- app.js
+++ app.js
@@ -7,3 +7,29 @@ navigator.serviceWorker.register(
 );
 
 }
+
+function loadIngredientStockAlertTemplate(){
+
+  const template =
+    document.getElementById(
+      "ingredientStockAlertTemplate"
+    );
+
+
+  if(!template) return;
+
+
+  if(
+    document.getElementById(
+      "ingredientStockAlertModal"
+    )
+  ){
+    return;
+  }
+
+
+  document.body.appendChild(
+    template.content.cloneNode(true)
+  );
+
+}
