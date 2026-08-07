b504910230261e9d401d920baf9b20361b5b4551
bangmun07-source <bangmun07@gmail.com>
2026-08-08 04:37:51 +0700

    Update supabase-client.js

--- supabase-client.js
+++ supabase-client.js
@@ -1,1 +1,9 @@
+const SUPABASE_URL = "https://glbyqlibiapiorztgxee.supabase.co";
 
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdsYnlxbGliaWFwaW9yenRneGVlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMxNDQ5NTIsImV4cCI6MjA5ODcyMDk1Mn0.4EMqkY3hbMJMwqtwwS3dv1MM1HDueuCrn9BI7uNsqss";
+
+
+const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
