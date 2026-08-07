93d58b9f8524d0347032d2d148a3e5b36919a472
bangmun07-source <bangmun07@gmail.com>
2026-08-07 16:38:38 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,28 @@
+const CACHE_NAME = "sorra-pos-v1";
 
+
+self.addEventListener(
+"install",
+event => {
+
+self.skipWaiting();
+
+});
+
+
+
+self.addEventListener(
+"activate",
+event => {
+
+clients.claim();
+
+});
+
+
+
+self.addEventListener(
+"fetch",
+event => {
+
+});
