ea9f633b80aa1cf257fc0c8452e00c988e6b1977
bangmun07-source <bangmun07@gmail.com>
2026-08-08 04:41:18 +0700

    Update rpc.js

--- rpc.js
+++ rpc.js
@@ -1,1 +1,24 @@
+async function checkoutTransaction(payload){
 
+  const { data, error } =
+    await supabaseClient.rpc(
+      "checkout_transaction",
+      {
+        p_payload: payload
+      }
+    );
+
+
+  if(error){
+
+    console.error(
+      "checkout_transaction error:",
+      error
+    );
+
+    throw error;
+  }
+
+
+  return data;
+}
